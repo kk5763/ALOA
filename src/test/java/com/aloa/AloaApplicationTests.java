0a322110b453713525472cbb23f146e31c96d1de
gong <yolip51@naver.com>
2017-03-13 10:24:28 +0900

    DB연결 테스트(외부jar확인)-크게 신경 안 써도 될 부분

--- src/test/java/com/aloa/AloaApplicationTests.java
+++ src/test/java/com/aloa/AloaApplicationTests.java
@@ -1,7 +1,13 @@
 package com.aloa;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +17,23 @@ public class AloaApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	//db연결 테스트
+	
+	@Autowired
+	private DataSource ds;
+	
+	@Test
+	public void testConn() throws SQLException{
+		System.out.println("ds : " + ds);
+	}
+	
+	@Autowired
+	private SqlSessionFactory sqlSession;
+	
+	@Test
+	public void testSession(){
+		System.out.println("sqlSession : " + sqlSession);
 	}
 
 }
