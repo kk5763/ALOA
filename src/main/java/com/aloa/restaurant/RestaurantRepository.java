5a68c6a479f7f9565b3a82723d710dcf8727137b
Lsh <sbxjdnjs13@naver.com>
2017-03-22 05:12:53 +0900

    1.맛집들을 List로 받는 dao 코드 구현 --------------------------- 과제 1.Account와 다른 테이블과의 참조키 구현 2.실제 view에 필요한 데이터를 파악하여 테이블 수정 및 보완   - 민이형(프론트팀장)과 토의 3.맛집추가,리뷰쓰기 등등 데이터를 push하는 작업에 대한 구현. 4.로그인 기능 구현 5.페이지 보안 처리 6.소셜 로그인 연동 7.예약시스템 추가 8.관리자 페이지 구현(데이터를 이용하여 정확한 뷰 생성) 9.View페이지의 이미지 효과에 대한 구현   - 민이형(프론트팀장)과 토의

--- src/main/java/com/aloa/restaurant/RestaurantRepository.java
+++ src/main/java/com/aloa/restaurant/RestaurantRepository.java
@@ -1,7 +1,10 @@
 package com.aloa.restaurant;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant,Long>{
 	Restaurant findByResName(String resName);
+	List<Restaurant> findAll();
 }
