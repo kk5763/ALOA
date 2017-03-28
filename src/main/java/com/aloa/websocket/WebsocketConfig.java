1d1137199a8308904f0ce4a30d896444d1a5f24f
406-05\user <kk57633@naver.com>
2017-03-28 21:23:48 +0900

    websocket text

---
+++ src/main/java/com/aloa/websocket/WebsocketConfig.java
@@ -0,0 +1,23 @@
+package com.aloa.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/websocket").withSockJS();
+	}
+	
+	@Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+}
